cb100480d1356d7b88f4545c19e09daba7fe4a8b
Ethan Supercomputer <ethan.walker830@gmail.com>
2021-11-02 19:46:10 -0600

    finished first pass, things run

--- rnn_gmm/model.py
+++ rnn_gmm/model.py
@@ -1,6 +1,10 @@
 import torch 
 import torch.nn as nn
 import torch.distributions as D
+import torch.nn.functional as F
+
+import numpy as np
+
 from gmm import GaussianMixture
 from rnn_gmm.utils import MLP
 
@@ -10,44 +14,58 @@ class RnnGmm(nn.Module):
     super(RnnGmm, self).__init__()
     self.rnn = nn.RNN(2*dim+1, hidden_dim, num_layers, batch_first=True)
     self.hidden_dim = hidden_dim
+    self.num_layers = num_layers
     self.device = device
-    self.mlp = MLP(dim*hidden_dim, dim*hidden_dim*2, 2*n_comps)
+    self.mlp = MLP(hidden_dim, hidden_dim*4, 2*n_comps)
     self.n_comps = n_comps
+    self.dim = dim
   
-  def gmm(self, means, covs):
-    # self.means = torch.randn(n_comps, self.gmm_dim).to(device)
-    # self.covs = torch.ones(n_comps, self.gmm_dim).to(device)
+  # def gmm(self, means, covs):
+  #   # self.means = torch.randn(n_comps, self.gmm_dim).to(device)
+  #   # self.covs = torch.ones(n_comps, self.gmm_dim).to(device)
+  #   covs = torch.abs(covs) + 1.
+  #   mix = D.Categorical(torch.ones(self.n_comps,).to(self.device))
+  #   norm = D.Normal(means, covs)
+  #   comp = D.Independent(norm, 1)
+  #   prior = D.MixtureSameFamily(mix, comp)
+  #   return prior
+  
+  def log_prob(self, x, means, covs):
+    prec = torch.rsqrt(F.softplus(covs))
+    log_p = torch.sum((means*means + x*x - 2*x*means) / (prec**2), dim=1, keepdim=True)
+    log_det = torch.sum(torch.log(prec), dim=1, keepdim=True)
 
-    mix = D.Categorical(torch.ones(self.n_comps,).to(self.device))
-    comp = D.Independent(D.Normal(means, covs), 1)
-    prior = D.MixtureSameFamily(mix, comp)
-    return prior
+    return -.5 * (self.dim * np.log(2.*np.pi) + log_p) + log_det
+
+
 
   def forward(self, x, mask):
     N = x.size(0)
+    x = x.unsqueeze(1)
     x_o = x*mask
-    x_o_sq = torch.sqeeze(x_o)
+    x_o_sq = torch.squeeze(x_o)
     masks = mask.repeat(N,1)
-    h = torch.zeros(self.hidden_dim)
 
     bool_mask = torch.gt(1-mask, 0)
     x_u = x[:,:,bool_mask]
+
+    p = 0.
 
     # initial state
     concat = torch.cat(
       (torch.zeros_like(x_u[:,:,0]),x_o_sq,masks), dim=1
     ).unsqueeze(1)
-    o, h = self.rnn(concat, h)
-    means, covs = self.mlp(o).chunk(2,dim=1)
-    p = self.gmm(means,covs).log_prob(x_u[:,:,0])
+    o, h = self.rnn(concat)
+    means, covs = self.mlp(o.squeeze()).chunk(2,dim=1)
+    p = self.log_prob(x_u[:,:,0], means, covs)
     
 
     # find probs for the rest of the states
     for i in range(1,x_u.size(-1)):
       concat = torch.cat((x_u[:,:,i-1],x_o_sq,masks), dim=1).unsqueeze(1)
       o, h = self.rnn(concat, h)
-      means, covs = self.mlp(o).chunk(2,dim=1)
-      p += self.gmm(means,covs).log_prob(x_u[:,:,i])
+      means, covs = self.mlp(o.squeeze()).chunk(2,dim=1)
+      p += self.log_prob(x_u[:,:,i], means, covs)
 
 
     return o, p
--- train.py
+++ train.py
@@ -4,28 +4,29 @@ from torchvision.transforms import transforms
 
 import pickle
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 from rnn_gmm import RnnGmm
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-
+torch.autograd.set_detect_anomaly(True)
 
 def get_mnist(batch_size):
   transform = transforms.Compose(
     [
       transforms.ToTensor(),
       transforms.Normalize((0.1307,), (0.30811,)),
-      transforms.Lambda(lambda x: torch.flatten(x)),
-      transforms.Lambda(lambda x: torch.unsqueeze(x,0))
+      transforms.Lambda(lambda x: torch.flatten(x))
+      # transforms.Lambda(lambda x: torch.unsqueeze(x,0))
     ]
   )
 
   train_set = tv.datasets.MNIST(
-    '../data/', train=True, download=True, transform=transform
+    'data/', train=True, download=True, transform=transform
   )
   test_set = tv.datasets.MNIST(
-    '../data/', train=False, download=True, transform=transform
+    'data/', train=False, download=True, transform=transform
   )
 
   train_loader = torch.utils.data.DataLoader(
@@ -42,15 +43,18 @@ def train(model, train_loader, lr, num_epochs=10, save_iters=5):
   optimizer = torch.optim.SGD(model.parameters(), lr=lr)
   losses = []
   running_loss = 0
+  len_epoch = len(train_loader)
 
-  with tqdm(total=num_epochs) as progress:
+  with tqdm(total=num_epochs*len(train_loader)) as progress:
     for epoch in range(num_epochs):
       for i , (x, y) in enumerate(train_loader):
         x = x.to(device)
         y = y.to(device)
 
+        mask = torch.round(torch.rand(x.size(1))).to(device)
+
         optimizer.zero_grad()
-        o, loss = model(x)
+        o, loss = model(x, mask)
         loss = -loss.mean()
         loss.backward()
         optimizer.step()
@@ -62,7 +66,7 @@ def train(model, train_loader, lr, num_epochs=10, save_iters=5):
           )
         progress.update()
 
-        if i % 100 == 99:
+        if (i + epoch*len_epoch) % 100 == 99:
           losses.append(running_loss / 100)
           running_loss = 0
           
@@ -81,13 +85,12 @@ def train(model, train_loader, lr, num_epochs=10, save_iters=5):
 
 
 if __name__ == '__main__':
-  batch_size = 512
-  res_net_layers = 10
+  batch_size = 1024
 
   train_loader, test_loader, train_set, test_set = get_mnist(batch_size)
 
   # for layers in [2**i for i in [2,3,4,5,6]]:
-  for layers in [4]:
+  for layers in [4,6]:
     for lr in [1e-5, 1e-10, 1e-8]:
       n_epochs = 25
       
@@ -105,11 +108,17 @@ if __name__ == '__main__':
 
       # model.load_state_dict(torch.load('chkpt/test.tar')['model_state_dict'])
 
-      samples = model.sample(100).detach().cpu().numpy()
+      # samples = model.sample(100).detach().cpu().numpy()
       out_dict = {
-        'sample': samples,
+        # 'sample': samples,
         'losses': losses
       }
 
       with open(f'chkpt/rnngmm_{lr:.8}.pickle','wb') as out:
         pickle.dump(out_dict, out)
+      
+      try:
+        plt.plot(losses)
+        plt.savefig('chkpt/images/rnngmm_{lr:.8}.png')
+      except:
+        print('there was an exception')
